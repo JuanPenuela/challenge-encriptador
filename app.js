6bed7d62eb953f2f4e898db57d8520854ea90073
JuanPenuela <juanchopenuela@gmail.com>
2024-04-07 17:29:31 -0500

    Agregando funcionalidades de encriptado y desencriptado

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 function encriptar(){
 
-    let texto = document.getElementById("texto").value;
+    let texto = document.getElementById('texto').value;
+
+    let textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+
+    let convertido = document.getElementById('cambiado-texto');
+
+    convertido.innerHTML = textoCifrado;
+
+    document.getElementById('imagen-mensaje').setAttribute('hidden','true');
+    document.querySelector('.texto-mensaje').setAttribute('hidden','true');
+    document.querySelector('.mensaje-instruccion').setAttribute('hidden','true');
 
     console.log(texto);
+}
 
+function desencriptar(){
+
+    let texto = document.getElementById('texto').value;
+
+    let textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+
+    let convertido = document.getElementById('cambiado-texto');
+
+    convertido.innerHTML = textoCifrado;
+
+    document.getElementById('imagen-mensaje').setAttribute('hidden','true');
+    document.querySelector('.texto-mensaje').setAttribute('hidden','true');
+    document.querySelector('.mensaje-instruccion').setAttribute('hidden','true');
+
+    console.log(texto);
 }
